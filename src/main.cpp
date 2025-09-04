5d932f6548d8ce7fa1897ea8755cd362f0447eac
laizhizhi <sam950706@gmail.com>
2025-09-05 01:52:01 +0800

    未加上切換傳輸協議前的最新版

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,10 @@
 #include <SPI.h>
 #include <LoRa.h>
 #define DHTPIN A13
-//---------------------------------------- LoRa Pin / GPIO configuration.
 #define nss 5
 #define rst 14
 #define lora_pin 2
-//----------------------------------------
+
 const char* ssid = "B2-7";       // 無線網路基地台的SSID
 const char* password = "00000000";       // 無線網路基地台的密碼
 // MQTT部分 • 函式區引用及參數設定
@@ -26,8 +25,6 @@ WiFiClient espClient;  // 建立WiFiClient物件
 PubSubClient client(espClient);  // 建立WiFiClient的物件作為其參數 •
 
 String Incoming = "";                   //LoRa incoming
-byte LocalAddress = 0x01;               //--> address of this device (Master Address).
-byte Destination_ESP32_Slave_1 = 0x02;  //--> destination to send to Slave 1 (ESP32).// Variable declaration for Millis/Timer.
 unsigned long previousMillis_SendMSG = 0;
 
 DHT dht(DHTPIN , DHT22);
@@ -35,8 +32,12 @@ float temperature, humidity;  // 溫度值,濕度值
 Adafruit_CCS811 CO2;          //CO2 SENSOR物件
 int dioxide;                //CO2的數值
 
-
-void connect_wifi() {  // 連線到Wi-Fi基地台
+void readData() {       // 讀取溫濕度、二氧化碳濃度數據
+  temperature = dht.readTemperature();
+  humidity = dht.readHumidity();
+  dioxide = CO2.geteCO2();
+} 
+void connect_wifi() {   // 連線到Wi-Fi基地台
       Serial.print("Connecting");
       Serial.println(ssid);
       WiFi.begin(ssid, password);
@@ -46,7 +47,7 @@ void connect_wifi() {  // 連線到Wi-Fi基地台
       }
       Serial.println("Wi-Fi已連線");
     }
-void connected_mqtt() {
+void connected_mqtt() { // 連線到MQTT經紀人
     while (!client.connected()) {
       Serial.println("Attempting MQTT connection...");
       if (client.connect(clientID.c_str(), mqttUser, mqttPassword)) {
@@ -59,7 +60,7 @@ void connected_mqtt() {
       }
     }    
 }
-void publish_mqtt() {
+void publish_mqtt() {   // 發佈數據到MQTT經紀人
   while (!client.connected()) {
     connected_mqtt();
   }
@@ -76,7 +77,7 @@ void publish_mqtt() {
   }
   delay(10000);
 }
-void set_lora() {
+void set_lora() {       // LoRa初始化
   LoRa.setPins(nss, rst, lora_pin);
 
   Serial.println("Start LoRa init...");
@@ -86,34 +87,23 @@ void set_lora() {
   }
   Serial.println("LoRa init succeeded.");
 }
-void readData() {
-  temperature = dht.readTemperature();
-  humidity = dht.readHumidity();
-  dioxide = CO2.geteCO2();
-} 
-void sendMessage(String Outgoing, byte Destination) {
-  LoRa.beginPacket();             //--> start packet
-  LoRa.write(Destination);        //--> add destination address
-  LoRa.write(LocalAddress);       //--> add sender address
-  LoRa.write(Outgoing.length());  //--> add payload length
-  LoRa.print(Outgoing);           //--> add payload
-  LoRa.endPacket();               //--> finish packet and send it 
-}
-void Lora_send(){
+void Lora_send(){                   // LoRa發送數據
   String merge_to_CSV = String(temperature) + "," + String(humidity) + "," + String(dioxide);
   unsigned long currentMillis_SendMSG = millis();
   if (currentMillis_SendMSG - previousMillis_SendMSG >= 10000) {
     previousMillis_SendMSG = currentMillis_SendMSG;
-    //::::::::::::::::: Condition for sending message / command data to Slave 1 (ESP32 Slave 1).
+
       Serial.println();
       Serial.println("Temperature :" + String(temperature));
       Serial.println("Humidity :" + String(humidity));
       if(CO2.available()){
-      if(!CO2.readData()){
-        Serial.println("CO2_level :" + String(dioxide));
-        sendMessage(merge_to_CSV, Destination_ESP32_Slave_1);
+        if(!CO2.readData()){
+          Serial.println("CO2_level :" + String(dioxide));
         }
       }
+        LoRa.beginPacket();             //--> start packet
+        LoRa.print(merge_to_CSV);       //--> add payload
+        LoRa.endPacket();               //--> finish packet and send it 
   }
   //----------------------------------------
 }
@@ -167,10 +157,10 @@ void setup() {
   Serial.begin(9600);   // 啟動序列埠
   connect_wifi();       // 執行 Wi-Fi 連線
   dht.begin();          //DHT22初始化
-  CO2.begin(); //啟用CCS811
+  CO2.begin();          //啟用CCS811
   client.setServer(mqttServer, mqttPort);  // 設定 MQTT 經紀人
   connected_mqtt();
-  // set_lora();
+  set_lora();
 }
 
 void loop() {
@@ -182,6 +172,7 @@ void loop() {
   //     Serial.println("CO2: " + String(dioxide) + " ppm");
   //   }
   // }
-  publish_mqtt();
-  // Lora_send();
+
+  // publish_mqtt();
+  Lora_send(); 
 }
